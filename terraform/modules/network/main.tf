7134a1b13d5b3ee4dc48dc0e912e5eb39acdd494
Michael Hundelt <michael.hundelt@gmail.com>
2026-03-06 13:52:47 -0600

    Add documentation for network module in main.tf
    Added a comment block describing the network module and its resources.

---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,11 @@
+# Network Module
+
+# This module will provision the VPC and subnet architecture
+# for the Aegis-Deploy platform.
+
+# Resources typically include:
+# - VPC
+# - Public Subnets
+# - Private Subnets
+# - NAT Gateway
+# - Security Groups
